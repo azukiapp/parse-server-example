5f0967569a4404f3efbfb8819f01b3f29defaa95
Marcus Gadbem <mcsgad@gmail.com>
2016-02-25 20:00:23 -0300

    Removing all unecessary files to run without azk

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '' //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || 'myMasterKey' //Add your master key here. Keep it secret!
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -35,11 +35,7 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.get('/deployment', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/deployment.html'));
-});
-
 var port = process.env.PORT || 1337;
 app.listen(port, function() {
-  console.log(`* parse-server-example running on port ${port}.`);
+  console.log('* parse-server-example running on port '+ port + '.');
 });
